d712d352278e9c67a4e03723670e92f8108b3709
Abdi8090 <cabdikariincabdullahimaxamad@gmail.com>
2026-02-15 10:28:30 +0200

    added exercise 4

---
+++ exercise-4/main-4.js
@@ -0,0 +1,20 @@
+
+// qeybta 1aad ee leyliga 
+
+function add(a,b){
+    console.log( + a + b)
+}
+add(10,50)
+add(15,30)
+add(19,55)
+add(34,50)
+add(16,70)
+
+// qeybta 2aad ee leyliga 
+const greet =function (name) {
+    console.log("Hello dear: "+ name)
+}
+greet("Abdu");
+greet("Abdalla")
+greet("Mohamed");
+greet("Leyla")
