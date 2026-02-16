d3183145f66d2caf5bd58879814897db8771af78
Abdi8090 <cabdikariincabdullahimaxamad@gmail.com>
2026-02-16 08:33:28 +0200

    added exercise-7

---
+++ exercise-7/main.js
@@ -0,0 +1,14 @@
+let color = ["Red", "green" ,"black", "yellow",];
+console.log(color[0])
+console.log(color[1])
+console.log(color[2])
+console.log(color[3])
+
+color.push("chocolate");
+console.log(color);
+color.pop();
+console.log(color)
+color.unshift("Tomato");
+console.log(color)
+color.shift()
+console.log(color);
