7875c0e90327670d304c296fd72ca5e2b36f3f91
Abdi8090 <cabdikariincabdullahimaxamad@gmail.com>
2026-02-19 11:16:06 +0200

    added exercise-9

---
+++ exercise-9/main.js
@@ -0,0 +1,10 @@
+let car = {
+    name : "Toyoto v8",
+    model : 2020,
+    year : 2025,
+    start : function(){
+        console.log("the car has started")
+    }
+}
+console.log(car)
+console.log(car.start())
