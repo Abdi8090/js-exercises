2b81c7c564d97076d6bd07f49a180625a532f10d
Abdi8090 <cabdikariincabdullahimaxamad@gmail.com>
2026-02-19 11:44:39 +0200

    added exercise

---
+++ exercise-10/main.js
@@ -0,0 +1,16 @@
+let library = [
+    {
+        name: "hanaadkaab",
+        author: "Abdullahi",
+        year: 2020 ,
+    },
+    {
+        name: "jiheyguushaada",
+        author: "mohamed",
+        year: 2022 ,
+    },
+]
+console.log(library)
+
+console.log(library[0].name)
+console.log(library[1].author)
